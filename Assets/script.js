84c19f3383fda0b124010972f5ade12833aa826e
Semi <Semiharris3@gmail.com>
2022-01-09 18:27:45 -0500

    Displayed the time boxs and gave them background color depending on the time

--- Assets/script.js
+++ Assets/script.js
@@ -18,3 +18,86 @@ setInterval(function(){
     displayCurrent() 
 
 }, 1000);
+
+/*Display Work Hours*/
+var displayWorkHour = function(){
+    for (var i = 0; i < time.length; i++){
+
+        var conatainer = $(".container")
+        var timeBlock = $("<div class= 'row'>").appendTo(conatainer)
+
+        if (time[i] <= 5){
+            
+            $("<P class='time-block col-1'>").text((time[i]) + 'PM').appendTo(timeBlock);
+    
+            $("<textarea class= 'col-10'>").addClass('timeText' + i).appendTo(timeBlock);
+        
+            $("<button class= 'saveBtn col-1'>").on( "click", function(){save()}).appendTo(timeBlock)
+        }else if (time[i] == 12){
+
+            $("<P class='time-block col-1'>").text((time[i]) + 'PM').appendTo(timeBlock);
+    
+            $("<textarea class= 'col-10'>").addClass('timeText' + i).appendTo(timeBlock);
+        
+            $("<button class= 'saveBtn col-1'>").on( "click", function(){save()}).appendTo(timeBlock)
+        }else{
+
+            $("<P class='time-block col-1'>").text(time[i] + 'AM').appendTo(timeBlock);
+    
+            $("<textarea class= 'col-10'>").addClass('timeText' + i).appendTo(timeBlock);
+
+            $("<button class= 'saveBtn col-1'>").on( "click", function(){save()}).appendTo(timeBlock);
+        }
+    
+
+    }
+    $("<span class='oi oi-account-login'>").appendTo($('.saveBtn'))
+    
+    load()
+    backgroundColor()
+};
+
+/*This adds 12 hours to the pm times*/
+var backgroundTime = function(i) {
+    if ((time[i] + 12) < currentHour){
+
+        var timeText = $('.timeText' + i)
+        timeText.removeClass('future present')
+        timeText.addClass('past')
+       }else if ((time[i] + 12) > currentHour){                     
+        var timeText = $('.timeText' + i)
+        timeText.removeClass('past present')
+        timeText.addClass('future')
+       }else{
+        var timeText = $('.timeText' + i)
+        timeText.removeClass('past future')
+        timeText.addClass('present')    
+       }   
+} 
+
+/*Adds background color to the textarea*/
+var backgroundColor = function (){
+    for ( var i = 0; i < time.length; i++){
+       if (time[i] < 6){
+
+        backgroundTime(i)      
+        }else if (time[i] < currentHour){
+
+        var timeText = $('.timeText' + i)
+        timeText.removeClass('future present')
+        timeText.addClass('past')
+
+       }else if (time[i] > currentHour) {
+
+        var timeText = $('.timeText' + i)
+        timeText.removeClass('past present')
+        timeText.addClass('future')
+
+       }else{
+        var timeText = $('.timeText' + i)
+        timeText.removeClass('past future')
+        timeText.addClass('present')
+
+       }
+    }
+};
